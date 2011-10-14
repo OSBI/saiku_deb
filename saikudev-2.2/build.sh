3f4410feb09638f28b21b2deb301875240418ead
Tom Barber <tom@analytical-labs.com>
2011-10-14 15:37:46 +0100

    add build script

--- saikudev-2.2/build.sh
+++ saikudev-2.2/build.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-$echo "blah" | dh_make --createorig --single
-dpkg-buildpackage
+(cd ${WORKSPACE}/saikudev-2.2 && $echo "blah" | dh_make --createorig --single
+(cd ${WORKSPACE}/saikudev-2.2 && dpkg-buildpackage)
